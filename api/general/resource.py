7719bc5a2df38a1a9975e93218970b5e45e1e86a
DavidFauzi <davidfauzi29@gmail.com>
2022-01-02 12:25:18 +0700

    feat : add general info API functionality
    Using Requests module from python, fetch JSON data from goverment covid19 API, then parse the data so that it fits into the response needed for this API

--- api/general/resource.py
+++ api/general/resource.py
@@ -1,5 +1,5 @@
-from flask import Blueprint,request
-
+from flask import Blueprint, request
+import requests
 general = Blueprint("general", __name__, url_prefix="/")
 
 """
@@ -8,7 +8,35 @@ Method: GET
 Description: Entry point for all API, provide general information of covid cases.
 Response Body (JSON)
 """
+
+
 @general.get('')
 def get_general_info():
-    response = {"message":"covid cases general information"}
-    return response,200
+    url = "https://data.covid19.go.id/public/api/update.json"
+    res = requests.get(url)
+    data = res.json()
+    total_positive = data['update']['total']['jumlah_positif']
+    total_recovered = data['update']['total']['jumlah_sembuh']
+    total_deaths = data['update']['total']['jumlah_meninggal']
+    total_active = total_positive - total_recovered - total_deaths
+    
+    new_positive = data['update']['penambahan']['jumlah_positif']
+    new_recovered = data['update']['penambahan']['jumlah_sembuh']
+    new_deaths = data['update']['penambahan']['jumlah_meninggal']
+    new_active = new_positive - new_recovered - new_deaths
+    
+    response = {
+        "ok": True,
+        "data": {
+            "total_positive": total_positive,
+            "total_recovered": total_recovered,
+            "total_deaths": total_deaths,
+            "total_active": total_active,
+            "new_positive": new_positive,
+            "new_recovered": new_recovered,
+            "new_deaths": new_deaths,
+            "new_active": new_active
+            },
+        "message": "Get covid cases general information success"
+    }
+    return response, 200
