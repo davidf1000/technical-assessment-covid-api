217940df90336963bbcc28b1b957e993e177f056
DavidFauzi <davidfauzi29@gmail.com>
2022-01-03 04:11:55 +0700

    fix : handle dict error

--- api/general/resource.py
+++ api/general/resource.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 import requests
 
-from api.constants.http_status_codes import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
+from api.constants.http_status_codes import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 general = Blueprint("general", __name__, url_prefix="/")
 
 """
@@ -24,30 +24,36 @@ def get_general_info():
         }
         return response,HTTP_500_INTERNAL_SERVER_ERROR
     data = res.json()
-    
-    # Check if exists 
-    total_positive = data['update']['total']['jumlah_positif']
-    total_recovered = data['update']['total']['jumlah_sembuh']
-    total_deaths = data['update']['total']['jumlah_meninggal']
-    total_active = total_positive - total_recovered - total_deaths
-    
-    new_positive = data['update']['penambahan']['jumlah_positif']
-    new_recovered = data['update']['penambahan']['jumlah_sembuh']
-    new_deaths = data['update']['penambahan']['jumlah_meninggal']
-    new_active = new_positive - new_recovered - new_deaths
-    
-    response = {
-        "ok": True,
-        "data": {
-            "total_positive": total_positive,
-            "total_recovered": total_recovered,
-            "total_deaths": total_deaths,
-            "total_active": total_active,
-            "new_positive": new_positive,
-            "new_recovered": new_recovered,
-            "new_deaths": new_deaths,
-            "new_active": new_active
-            },
-        "message": "Request Successfull"     
-    }
-    return response, HTTP_200_OK
+    # Catch Error if wrong dict structure
+    try:
+        total_positive = data['update']['total']['jumlah_positif']
+        total_recovered = data['update']['total']['jumlah_sembuh']
+        total_deaths = data['update']['total']['jumlah_meninggal']
+        total_active = total_positive - total_recovered - total_deaths
+        
+        new_positive = data['update']['penambahan']['jumlah_positif']
+        new_recovered = data['update']['penambahan']['jumlah_sembuh']
+        new_deaths = data['update']['penambahan']['jumlah_meninggal']
+        new_active = new_positive - new_recovered - new_deaths
+        
+        response = {
+            "ok": True,
+            "data": {
+                "total_positive": total_positive,
+                "total_recovered": total_recovered,
+                "total_deaths": total_deaths,
+                "total_active": total_active,
+                "new_positive": new_positive,
+                "new_recovered": new_recovered,
+                "new_deaths": new_deaths,
+                "new_active": new_active
+                },
+            "message": "Request Successfull"     
+        }
+        return response, HTTP_200_OK        
+    except Exception as e:
+        response = {
+            "ok" : False,
+            "message" : "Error on getting information"
+        }
+        return response,HTTP_404_NOT_FOUND
