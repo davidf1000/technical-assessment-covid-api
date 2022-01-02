0fd07bca201f8b697ae35800087356659251efb9
DavidFauzi <davidfauzi29@gmail.com>
2022-01-03 03:00:36 +0700

    feat : add get daily of provided year API functionality

--- api/daily/resource.py
+++ api/daily/resource.py
@@ -84,13 +84,46 @@ Response Body (JSON), example: /daily/2020
 
 @daily.get('/<year>')
 def get_daily_data_of_provided_year(year):
-    since = request.args.get('since')
-    upto = request.args.get('upto')
+    # GET json data
+    url = "https://data.covid19.go.id/public/api/update.json"
+    res = requests.get(url)
+    list_daily = res.json()['update']['harian']
+    # Filter by year first before further processing
+    list_daily = [x for x in list_daily if parse(x['key_as_string']).year == int(year)]        
+    # Find earliest daily data and current date
+    earliest = parse(min(list_daily, key=lambda x: parse(
+        x['key_as_string']))['key_as_string'])
+    current = datetime.datetime.now()
+    # Get string query, if not found, use earliest data (since) or use current data (upto)
+    since = parse(request.args.get('since').replace('.', '-')
+                  ) if request.args.get('since') is not None else earliest
+    upto = parse(request.args.get('upto').replace('.', '-') # Deltatime 1 day ahead to ensure that 'upto' date is included in filter
+                 ) + datetime.timedelta(days=1) if request.args.get('upto') is not None else current
+    # Filter daily data that fits inside year, month, and day range
+    list_daily = [x for x in list_daily if since.timestamp() <= parse(
+        x['key_as_string']).timestamp() <= upto.timestamp()]
+    # Restructure every element in list to fit the needed structure
+    # Create empty list
+    data = []
+    for item in list_daily:
+        date = parse(item["key_as_string"])
+        positive = item["jumlah_positif"]['value']
+        recovered = item["jumlah_sembuh"]['value']
+        deaths = item["jumlah_meninggal"]['value']
+        active = positive - recovered - deaths
+        data.append({
+            "date": f'{date.year}-{str(date.month).zfill(2)}-{str(date.day).zfill(2)}',
+            "positive": positive,
+            "recovered": recovered,
+            "deaths": deaths,
+            "active": active
+        })
+    # Sort ASC
+    data.sort(key=lambda x: x['date'])
     response = {
-        "message": "daily covid cases information of provided year",
-        "year": year,
-        "since": since,
-        "upto": upto
+        "ok": True,
+        "data": data,
+        "message": "Request Successfull"
     }
     return response, 200
 
@@ -111,15 +144,48 @@ Response Body (JSON), example: /daily/2020/05
 
 
 @daily.get('/<year>/<month>')
-def get_daily_data_of_provided_year_month(year, month):
-    since = request.args.get('since')
-    upto = request.args.get('upto')
+def get_daily_data_of_provided_year_month(year):
+    # GET json data
+    url = "https://data.covid19.go.id/public/api/update.json"
+    res = requests.get(url)
+    list_daily = res.json()['update']['harian']
+    # Filter by year and month first before further processing
+    list_daily = [x for x in list_daily if f"{parse(x['key_as_string']).year}-{parse(x['key_as_string']).month}" == f"{year}-{int(month)}"]
+    # Find earliest daily data and current date
+    earliest = parse(min(list_daily, key=lambda x: parse(
+        x['key_as_string']))['key_as_string'])
+    current = datetime.datetime.now()
+    # Get string query, if not found, use earliest data (since) or use current data (upto)
+    since = parse(request.args.get('since').replace('.', '-')
+                  ) if request.args.get('since') is not None else earliest
+    upto = parse(request.args.get('upto').replace('.', '-') # Deltatime 1 day ahead to ensure that 'upto' date is included in filter
+                 ) + datetime.timedelta(days=1) if request.args.get('upto') is not None else current
+    
+    # Filter daily data that fits inside year, month, and day range
+    list_daily = [x for x in list_daily if since.timestamp() <= parse(
+        x['key_as_string']).timestamp() <= upto.timestamp()]
+    # Restructure every element in list to fit the needed structure
+    # Create empty list
+    data = []
+    for item in list_daily:
+        date = parse(item["key_as_string"])
+        positive = item["jumlah_positif"]['value']
+        recovered = item["jumlah_sembuh"]['value']
+        deaths = item["jumlah_meninggal"]['value']
+        active = positive - recovered - deaths
+        data.append({
+            "date": f'{date.year}-{str(date.month).zfill(2)}-{str(date.day).zfill(2)}',
+            "positive": positive,
+            "recovered": recovered,
+            "deaths": deaths,
+            "active": active
+        })
+    # Sort ASC
+    data.sort(key=lambda x: x['date'])
     response = {
-        "message": "daily covid cases information in the provided year and month",
-        "year": year,
-        "month": month,
-        "since": since,
-        "upto": upto
+        "ok": True,
+        "data": data,
+        "message": "Request Successfull"
     }
     return response, 200
 
